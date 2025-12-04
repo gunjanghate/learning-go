95e86f37c336609ccec9afb1e8e72d76a5a1b475
gunjanghate <ghategunjan@gmail.com>
2025-12-05 00:11:58 +0530

    feat: night 0

---
+++ hellloworld&simplevalues/main.go
@@ -0,0 +1,22 @@
+///// Simple Values in Go /////
+// //intger
+// fmt.Println(42/8)
+
+// //float
+// fmt.Println(3.14)
+
+// // string
+// fmt.Println("Night 0 of learning Go!")
+
+// //boolean
+// fmt.Println(true)
+package main
+
+import "fmt"
+
+func main() {
+	time := "Night"
+	day := 0
+	activity := "learning Go"
+	fmt.Println(time, day, "of", activity)
+}
