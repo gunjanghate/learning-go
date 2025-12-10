6d73bba63b58879069c019fd40d10917c97719f1
gunjanghate <ghategunjan@gmail.com>
2025-12-10 13:22:14 +0530

    feat: adding about interfaces

--- enums&generics/main.go
+++ enums&generics/main.go
@@ -1,6 +1,26 @@
 package main
 
+import "fmt"
+
+
+// enumerated types
+
+type OrderStatus int
+
+const (
+	Received OrderStatus = iota // predefined constant starting from 0
+	Confirmed // 1
+	Prepared // 2
+	Delivered // 3
+)
+
+func changeOrderStatus(status OrderStatus){
+	fmt.Println("Changing order status to", status)
+}
+
 
 func main(){
 
+	changeOrderStatus(Confirmed)
 }
+
